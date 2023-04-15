9f249a1442e05dc0147259e14828407b72de894c
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-04-15 10:29:23 +0530

    courses route started

---
+++ src/mock-data/courseSyllabus.js
@@ -0,0 +1,117 @@
+export const courses = [
+    {
+        courseName: "React JS",
+        courseDescription: "Among the most popular JavaScript libraries, ReactJS brings with it rich features that make it an easy to use and popular tool among developers. React libraries are growing at an enormous rate, helping developers in building rich efficient front-end abstractions by writing less code and in lesser time as well!",
+        noOfChapters: 4,
+        chapters: [
+            {
+                chapterName: "HTML/CSS",
+                sections: [
+                    {
+                        sectionName: "HTML Introduction",
+                        topicsCovered: ["Why ReactJS is high in demand","Advantages of using ReactJS", "How does ReactJS works?", "Why Virtual DOM", "Setting up the Project."]
+                    },
+                    {
+                        sectionName: "HTML5 Introduction",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Designing a Web Page",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Page Formatting",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Text Items and Objects",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Creating Lists",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Links",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Images",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Basic Tables",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Forms",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Cascading StyleSheets",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Box model in CSS",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Flexbox in CSS",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Working with elements in CSS",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    }
+                ]
+            },
+
+            {
+                chapterName: "ReactJS",
+                sections: [
+                    {
+                        sectionName: "React JS Introduction",
+                        topicsCovered: ["Why ReactJS is high in demand","Advantages of using ReactJS", "How does ReactJS works?", "Why Virtual DOM", "Setting up the Project."]
+                    },
+                    {
+                        sectionName: "JSX-Introduction",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Components & Props",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Class Components",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Functional Components",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Handling API Calls",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Conditional Rendering",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Forms & Validations in react",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Authentication in React JS",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Routing in ReactJS",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "React Fragments, HOC & TypeChecking with Prop Types",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Redux in React",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Error Boundaries",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Best Practices in ReactJS",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "NodeJS & Node Package Manager (NPM)",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "GitHub",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },{
+                        sectionName: "Netlify",
+                        topicsCovered: ["What is JSX?", "Advantages of using ReactJS", "Javascript vs JSX - Differences", "Rules of writing JSX"]
+                    },
+                ]
+            },
+        ]
+    }
+]
